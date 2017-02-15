829772b604397249473d2ff461fccee0f4eb231b
Andrew Haines <andrew.haines@red-badger.com>
2017-02-15 11:19:42 +0000

    Work around incorrect handling of $ in patterns by maven-shade-plugin

--- shade/src/shade/core.clj
+++ shade/src/shade/core.clj
@@ -2,27 +2,48 @@
   (:require [clojure.java.io :as io]
             [clojure.string :as string])
   (:import [org.apache.maven.plugins.shade DefaultShader ShadeRequest]
-           [org.apache.maven.plugins.shade.relocation SimpleRelocator]
+           [org.apache.maven.plugins.shade.relocation Relocator]
+           [java.util.regex Pattern]
            [org.codehaus.plexus.logging Logger]
            [org.codehaus.plexus.logging.console ConsoleLogger]))
 
-(defn- namespace-pattern [namespace suffix]
-  (str (namespace-munge namespace) suffix))
+(defn- namespace->path [namespace]
+  (-> namespace
+      namespace-munge
+      (string/replace "." "/")))
 
-(defn- relocator [namespace shaded-namespace suffix]
-  (SimpleRelocator. (namespace-pattern namespace suffix)
-                    (namespace-pattern shaded-namespace suffix)
-                    []
-                    []))
+(defn- path-pattern [namespace]
+  (re-pattern (str #"(?<=^/?)"
+                   (namespace->path namespace)
+                   #"(?=[.$/]|__init(?:\.class)?$|$)")))
 
-(defn- relocators [[namespace shaded-namespace]]
-  (map #(relocator namespace shaded-namespace %) ["." "$" "__init"]))
+(defn- class-pattern [namespace]
+  (re-pattern (str #"^"
+                   (-> namespace namespace-munge Pattern/quote)
+                   #"(?=[.$]|__init$|$)")))
+
+(defn- relocator [[namespace shaded-namespace]]
+  (let [path-pattern (path-pattern namespace)
+        path-replacement (namespace->path shaded-namespace)
+        class-pattern (class-pattern namespace)
+        class-replacement (namespace-munge shaded-namespace)]
+    (reify Relocator
+      (canRelocatePath [this path]
+        (boolean (re-find path-pattern path)))
+      (canRelocateClass [this class]
+        (boolean (re-find class-pattern class)))
+      (relocatePath [this path]
+        (string/replace path path-pattern path-replacement))
+      (relocateClass [this class]
+        (string/replace class class-pattern class-replacement))
+      (applyToSourceContent [this source]
+        source))))
 
 (defn- shade-request [jar shaded-jar relocations]
   (doto (ShadeRequest.)
     (.setJars #{(io/as-file jar)})
     (.setUberJar (io/as-file shaded-jar))
-    (.setRelocators (mapcat relocators relocations))
+    (.setRelocators (map relocator relocations))
     (.setFilters [])
     (.setResourceTransformers [])
     (.setShadeSourcesContent false)))
