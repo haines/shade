a642fffa19a3a3eb38f85f9d313a7db3cce925a6
Emlyn Corrin <Emlyn.Corrin@microsoft.com>
2020-08-12 20:00:54 +0200

    update maven shade plugin

--- shade/project.clj
+++ shade/project.clj
@@ -4,4 +4,4 @@
   :scm {:dir ".."}
   :license {:name "Eclipse Public License" :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.apache.maven.plugins/maven-shade-plugin "3.0.0"]])
+                 [org.apache.maven.plugins/maven-shade-plugin "3.2.4"]])
